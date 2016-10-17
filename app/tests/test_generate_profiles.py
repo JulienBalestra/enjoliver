bfa976b9a9206e5add545188209ee22636a060ec
Julien Balestra <julien.balestra@blablacar.com>
2016-10-17 17:48:29 +0200

    Clean the generate profiles

--- app/tests/test_generate_profiles.py
+++ app/tests/test_generate_profiles.py
@@ -1,3 +1,4 @@
+import os
 from unittest import TestCase
 
 import re
@@ -7,12 +8,22 @@ from app import generate_profiles
 
 class TestGenerateProfiles(TestCase):
     gen = generate_profiles.GenerateProfiles
+    network_environment = "%s/misc/network-environment" % gen.bootcfg_path
 
     @classmethod
     def setUpClass(cls):
         cls.gen = generate_profiles.GenerateProfiles()
+        if os.path.isfile("%s" % cls.network_environment):
+            os.remove("%s" % cls.network_environment)
+
+    @classmethod
+    def tearDownClass(cls):
+        if os.path.isfile("%s" % cls.network_environment):
+            os.remove("%s" % cls.network_environment)
 
     def test_00_ip_address(self):
+        self.assertFalse(os.path.isfile("%s" % self.network_environment))
         ip = self.gen.ip_address
         match = re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", ip)
         self.assertIsNotNone(match)
+        self.assertTrue(os.path.isfile("%s" % self.network_environment))
