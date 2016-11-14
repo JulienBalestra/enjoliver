46ead3ca3c4a67c2a4b70f69fe72385ab76ea2cb
Julien Balestra <julien.balestra@blablacar.com>
2016-11-14 11:58:56 +0100

    Fix the trim by split

--- discoveryC/cmdline_test.go
+++ discoveryC/cmdline_test.go
@@ -5,7 +5,7 @@ import (
 	"os"
 )
 
-func TestParseCommandLine(t *testing.T) {
+func Test0ParseCommandLine(t *testing.T) {
 	//var c Config
 	var err error
 	var discoAddress = "http://127.0.0.1:5000"
@@ -15,7 +15,7 @@ func TestParseCommandLine(t *testing.T) {
 	if err != nil {
 		t.Fail()
 	}
-	CONF.ProcCmdline = "tests" + CONF.ProcCmdline
+	CONF.ProcCmdline = "tests" + CONF.ProcCmdline + "0"
 
 	bi, err := ParseCommandLine()
 	if err != nil {
@@ -25,6 +25,55 @@ func TestParseCommandLine(t *testing.T) {
 		t.Error(bi.Uuid)
 	}
 	if bi.Mac != "52:54:00:0d:bd:31" {
+		t.Error(bi.Mac)
+	}
+}
+
+func Test1ParseCommandLine(t *testing.T) {
+	//var c Config
+	var err error
+	var discoAddress = "http://127.0.0.1:5000"
+
+	os.Setenv("DISCOVERY_ADDRESS", discoAddress)
+	CONF, err = CreateConfig()
+	if err != nil {
+		t.Fail()
+	}
+	CONF.ProcCmdline = "tests" + CONF.ProcCmdline + "1"
+
+	bi, err := ParseCommandLine()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	if bi.Uuid != "ee378e7b-1575-425d-a6d6-e0dadb9f31a0" {
+		t.Error(bi.Uuid)
+	}
+	if bi.Mac != "52:54:00:90:c3:9f" {
+		t.Error(bi.Mac)
+	}
+}
+
+
+func Test2ParseCommandLine(t *testing.T) {
+	//var c Config
+	var err error
+	var discoAddress = "http://127.0.0.1:5000"
+
+	os.Setenv("DISCOVERY_ADDRESS", discoAddress)
+	CONF, err = CreateConfig()
+	if err != nil {
+		t.Fail()
+	}
+	CONF.ProcCmdline = "tests" + CONF.ProcCmdline + "2"
+
+	bi, err := ParseCommandLine()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	if bi.Uuid != "62b58ffd-7960-419a-8359-91def082191a" {
+		t.Error(bi.Uuid)
+	}
+	if bi.Mac != "52:54:00:71:83:ea" {
 		t.Error(bi.Mac)
 	}
 }
