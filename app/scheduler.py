e4e6c3b2983d53f46ffde77a871ff1ee4256fa7a
Julien Balestra <julien.balestra@blablacar.com>
2016-11-17 19:29:56 +0100

    Requirements for K8S

--- app/scheduler.py
+++ app/scheduler.py
@@ -256,7 +256,8 @@ class K8sControlPlaneScheduler(CommonScheduler):
                     "etcd_advertise_client_urls": "http://%s:2379" % nic[0],
                     "etcd_proxy": "on",
                     # K8s Control Plane
-                    "apiserver-count": self.control_plane_nb
+                    "k8s_apiserver_count": self.control_plane_nb,
+                    "k8s_advertise_ip": "%s" % nic[0],
                 }
             )
             self._gen.dumps()
