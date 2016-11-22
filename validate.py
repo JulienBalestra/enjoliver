601b46688966f49d07621283cc64ac42821ea71e
Julien Balestra <julien.balestra@blablacar.com>
2016-11-22 12:52:29 +0100

    Can validate the quality of the setup

---
+++ validate.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python
+import unittest
+
+import os
+
+
+class TestValidateBootcfgAssets(unittest.TestCase):
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    boothcfg = "%s/bootcfg" % cwd
+    assets = "%s/assets" % boothcfg
+
+    def test_cni(self):
+        rule = "%s/%s/serve" % (self.assets, self.test_cni.__name__.replace("test_", ""))
+        self.assertIn("cni.tar.gz", os.listdir(rule))
+
+    def test_coreos(self):
+        rule = "%s/%s/serve" % (self.assets, self.test_coreos.__name__.replace("test_", ""))
+        list_dir = os.listdir(rule)
+        self.assertIn("coreos_production_pxe.vmlinuz", list_dir)
+        self.assertIn("coreos_production_pxe_image.cpio.gz", list_dir)
+
+    def test_discoveryC(self):
+        rule = "%s/%s/serve" % (self.assets, self.test_discoveryC.__name__.replace("test_", ""))
+        list_dir = os.listdir(rule)
+        self.assertIn("discoveryC", list_dir)
+
+    def test_hyperkube(self):
+        rule = "%s/%s/serve" % (self.assets, self.test_hyperkube.__name__.replace("test_", ""))
+        list_dir = os.listdir(rule)
+        self.assertIn("hyperkube", list_dir)
+
+    def test_lldp(self):
+        rule = "%s/%s/serve" % (self.assets, self.test_lldp.__name__.replace("test_", ""))
+        list_dir = os.listdir(rule)
+        self.assertIn("static-aci-lldp-0.aci", list_dir)
+
+    def test_rkt(self):
+        rule = "%s/%s/serve" % (self.assets, self.test_rkt.__name__.replace("test_", ""))
+        list_dir = os.listdir(rule)
+        self.assertIn("rkt.tar.gz", list_dir)
+
+    def test_setup_network_environment(self):
+        rule = "%s/%s/serve" % (
+            self.assets, self.test_setup_network_environment.__name__.replace("test_", "").replace("_", "-"))
+        list_dir = os.listdir(rule)
+        self.assertIn("setup-network-environment", list_dir)
+
+
+if __name__ == '__main__':
+    unittest.main()
