004fea27b7575e651039cc3054c6229d11cab006
Julien Balestra <julien.balestra@blablacar.com>
2016-11-22 11:55:41 +0100

    Create the hyperkube static binary

---
+++ hyperkube/aci-builder-k8s/runlevels/build/10.build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -exv
+set -o pipefail
+. /dgr/bin/functions.sh
+
+isLevelEnabled "debug" && set -x
+
+export LANG=C
+export TERM=xterm
+export DEBIAN_FRONTEND=noninteractive
+export GODEBUG=netdns=cgo
+
+apt-get update -qq
+apt-get upgrade -y -qq
+apt-get install -y -qq git curl tar build-essential rsync
+
+curl -L https://storage.googleapis.com/golang/go1.7.3.linux-amd64.tar.gz -o /tmp/go.tar.gz
+tar -C /usr/local -xzvf /tmp/go.tar.gz
