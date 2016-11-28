f60d14a9ca81c2e6658af63f8449e6739cdff3cc
Julien Balestra <julien.balestra@blablacar.com>
2016-11-28 18:07:44 +0100

    Don't test the test please

---
+++ app/tests/euid/test_kvm_player.py
@@ -0,0 +1,22 @@
+import unittest
+
+import kvm_player
+
+
+class TestKernelVirtualMachinePlayer(kvm_player.KernelVirtualMachinePlayer):
+    @classmethod
+    def setUpClass(cls):
+        cls.check_requirements()
+        cls.set_api()
+        cls.set_bootcfg()
+        cls.set_dnsmasq()
+        cls.set_lldp()
+        cls.set_rack0()
+        cls.pause(5)
+
+    def test(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
