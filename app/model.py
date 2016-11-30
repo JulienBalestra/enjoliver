f035cacc3f5d94fdf442fc3f78926202e29b686d
Julien Balestra <julien.balestra@blablacar.com>
2016-11-30 15:41:26 +0100

    Inject properly the data with duplication checking

--- app/model.py
+++ app/model.py
@@ -57,42 +57,81 @@ class ChassisPort(Base):
         return "<%s: %s %s>" % (ChassisPort.__name__, self.mac, self.chassis_mac)
 
 
-def insert_data(session, discovery):
-    machine = Machine(
-        uuid=discovery["boot-info"]["uuid"]
-    )
-    if len(session.query(Machine).filter(Machine.uuid == machine.uuid).all()) != 0:
-        return False
+class Inject(object):
+    def __init__(self, session, discovery):
+        self.session = session
+        self.adds = 0
+        self.discovery = discovery
 
-    session.add(machine)
-    interfaces = []
-    for i in discovery["interfaces"]:
-        if i["mac"]:
-            interfaces.append(
-                MachineInterface(
-                    name=i["name"],
-                    netmask=i["netmask"],
-                    mac=i["mac"],
-                    ipv4=i["ipv4"],
-                    cidrv4=i["cidrv4"],
-                    as_boot=True if i["mac"] == discovery["boot-info"]["mac"] else False,
-                    machine_uuid=machine.uuid)
-            )
-    session.add_all(interfaces)
-    for j in discovery["lldp"]["data"]["interfaces"]:
-        chassis = Chassis(
-            name=j["chassis"]["name"],
-            mac=j["chassis"]["id"]
-        )
-        session.add(chassis)
-        for machine_interface in interfaces:
-            if machine_interface.name == j["name"]:
-                ports = ChassisPort(
-                    mac=j["port"]["id"],
-                    chassis_mac=chassis.mac,
-                    machine_interface_mac=machine_interface.mac
+        self.machine = self._machine()
+        self.interfaces = self._machine_interfaces()
+
+        self.chassis = self._chassis()
+        self.chassis_port = self._chassis_port()
+
+    def _machine(self):
+        uuid = self.discovery["boot-info"]["uuid"]
+        machine = self.session.query(Machine).filter(Machine.uuid == uuid).first()
+        if machine:
+            return machine
+        machine = Machine(uuid=uuid)
+        self.session.add(machine)
+        self.adds += 1
+        return machine
+
+    def _machine_interfaces(self):
+        m_interfaces = self.machine.interfaces
+        if m_interfaces:
+            return m_interfaces
+
+        for i in self.discovery["interfaces"]:
+            if i["mac"] and i["mac"] not in [k.mac for k in m_interfaces]:
+                m_interfaces.append(
+                    MachineInterface(
+                        name=i["name"],
+                        netmask=i["netmask"],
+                        mac=i["mac"],
+                        ipv4=i["ipv4"],
+                        cidrv4=i["cidrv4"],
+                        as_boot=True if i["mac"] == self.discovery["boot-info"]["mac"] else False,
+                        machine_uuid=self.machine.uuid)
                 )
-                session.add(ports)
+                self.adds += 1
 
-    session.commit()
-    return True
+        return m_interfaces
+
+    def _chassis(self):
+        chassis_list = []
+        for j in self.discovery["lldp"]["data"]["interfaces"]:
+            chassis = self.session.query(Chassis).filter(Chassis.mac == j["chassis"]["id"]).first()
+            if not chassis:
+                chassis = Chassis(
+                    name=j["chassis"]["name"],
+                    mac=j["chassis"]["id"]
+                )
+                self.session.add(chassis)
+                self.adds += 1
+            chassis_list.append(chassis)
+
+        return chassis_list
+
+    def _chassis_port(self):
+        chassis_port_list = []
+        for j in self.discovery["lldp"]["data"]["interfaces"]:
+            for machine_interface in self.machine.interfaces:
+                if machine_interface.name == j["name"] and \
+                        not self.session.query(ChassisPort.mac == j["port"]["id"]).first():
+                    chassis = self.session.query(Chassis).filter(Chassis.mac == j["chassis"]["id"]).first()
+                    chassis_port = ChassisPort(
+                        mac=j["port"]["id"],
+                        chassis_mac=chassis.mac,
+                        machine_interface_mac=machine_interface.mac
+                    )
+                    self.session.add(chassis_port)
+                    self.adds += 1
+                    chassis_port_list.append(chassis_port)
+        return chassis_port_list
+
+    def commit(self):
+        if self.adds != 0:
+            self.session.commit()
