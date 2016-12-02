9f20e84c9a4f117244b72c29f82d3d11892e50b2
Julien Balestra <julien.balestra@blablacar.com>
2016-12-02 15:55:13 +0100

    Store the logs outside the DataBase

--- app/tests/unit/test_model.py
+++ app/tests/unit/test_model.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import unittest
 
 from sqlalchemy import create_engine
@@ -11,6 +12,7 @@ from common import posts
 class TestModel(unittest.TestCase):
     unit_path = os.path.dirname(os.path.abspath(__file__))
     dbs_path = "%s/dbs" % unit_path
+    journal_journal_path = "%s/ignition_journal" % unit_path
     engine = None
 
     @classmethod
@@ -20,15 +22,24 @@ class TestModel(unittest.TestCase):
             os.remove(db)
         except OSError:
             pass
+        try:
+            shutil.rmtree(cls.journal_journal_path)
+        except OSError:
+            pass
         assert os.path.isfile(db) is False
         cls.engine = create_engine('sqlite:///%s' % db)
         model.Base.metadata.create_all(cls.engine)
+        fetch = crud.Fetch(cls.engine, cls.journal_journal_path)
+        assert fetch.get_all_interfaces() == []
+        assert fetch.get_all() == []
+        assert fetch.get_ignition_journal("") == []
+        assert fetch.get_ignition_journal(posts.M01["boot-info"]["uuid"]) == []
 
     # @unittest.skip("")
     def test_00(self):
-        i = crud.Inject(self.engine, posts.M01)
+        i = crud.Inject(self.engine, self.journal_journal_path, posts.M01)
         i.commit_and_close()
-        fetch = crud.Fetch(self.engine)
+        fetch = crud.Fetch(self.engine, self.journal_journal_path)
         interfaces = fetch.get_all_interfaces()
         self.assertEqual([
             {
@@ -48,9 +59,9 @@ class TestModel(unittest.TestCase):
 
     # @unittest.skip("")
     def test_00_1(self):
-        i = crud.Inject(self.engine, posts.M01)
+        i = crud.Inject(self.engine, self.journal_journal_path, posts.M01)
         i.commit_and_close()
-        fetch = crud.Fetch(self.engine)
+        fetch = crud.Fetch(self.engine, self.journal_journal_path)
         interfaces = fetch.get_all_interfaces()
         self.assertEqual([
             {
@@ -69,9 +80,9 @@ class TestModel(unittest.TestCase):
         self.assertEqual(len(journal), len(posts.M01["ignition-journal"]))
 
     def test_01(self):
-        i = crud.Inject(self.engine, posts.M02)
+        i = crud.Inject(self.engine, self.journal_journal_path,  posts.M02)
         i.commit_and_close()
-        fetch = crud.Fetch(self.engine)
+        fetch = crud.Fetch(self.engine, self.journal_journal_path)
         interfaces = fetch.get_all_interfaces()
         self.assertEqual(len(interfaces), 2)
         self.assertEqual(len(fetch.get_ignition_journal(posts.M02["boot-info"]["uuid"])), 39)
@@ -100,13 +111,13 @@ class TestModel(unittest.TestCase):
         fetch.close()
 
     def test_02(self):
-        m1 = crud.Inject(self.engine, posts.M01)
+        m1 = crud.Inject(self.engine, self.journal_journal_path,  posts.M01)
         m1.commit_and_close()
-        i = crud.Inject(self.engine, posts.M02)
+        i = crud.Inject(self.engine, self.journal_journal_path,   posts.M02)
         i.commit_and_close()
-        i = crud.Inject(self.engine, posts.M02)
+        i = crud.Inject(self.engine, self.journal_journal_path,   posts.M02)
         i.commit_and_close()
-        fetch = crud.Fetch(self.engine)
+        fetch = crud.Fetch(self.engine, self.journal_journal_path)
         interfaces = fetch.get_all_interfaces()
         self.assertEqual(len(interfaces), 2)
         self.assertEqual(len(fetch.get_ignition_journal(posts.M02["boot-info"]["uuid"])), 39)
@@ -121,43 +132,43 @@ class TestModel(unittest.TestCase):
         fetch.close()
 
     def test_03(self):
-        i = crud.Inject(self.engine, posts.M03)
+        i = crud.Inject(self.engine, self.journal_journal_path, posts.M03)
         i.commit_and_close()
-        fetch = crud.Fetch(self.engine)
+        fetch = crud.Fetch(self.engine, self.journal_journal_path)
         interfaces = fetch.get_all_interfaces()
         self.assertEqual(len(interfaces), 3)
         self.assertEqual(len(fetch.get_ignition_journal(posts.M03["boot-info"]["uuid"])), 39)
 
     def test_04(self):
         for p in posts.ALL:
-            i = crud.Inject(self.engine, p)
+            i = crud.Inject(self.engine, self.journal_journal_path,  p)
             i.commit_and_close()
-        fetch = crud.Fetch(self.engine)
+        fetch = crud.Fetch(self.engine, self.journal_journal_path)
         interfaces = fetch.get_all_interfaces()
         self.assertEqual(len(posts.ALL), len(interfaces))
 
     def test_05(self):
         for p in posts.ALL:
-            i = crud.Inject(self.engine, p)
+            i = crud.Inject(self.engine, self.journal_journal_path,  p)
             i.commit_and_close()
 
-        fetch = crud.Fetch(self.engine)
+        fetch = crud.Fetch(self.engine, self.journal_journal_path)
         interfaces = fetch.get_all_interfaces()
         self.assertEqual(len(posts.ALL), len(interfaces))
 
     def test_06(self):
-        i = crud.Inject(self.engine, posts.M16)
+        i = crud.Inject(self.engine, self.journal_journal_path,  posts.M16)
         i.commit_and_close()
 
     def test_07(self):
         with self.assertRaises(KeyError):
-            i = crud.Inject(self.engine, {
+            i = crud.Inject(self.engine, self.journal_journal_path,  {
                 u'boot-info': {},
                 u'lldp': {},
                 u'interfaces': []
             })
             i.commit_and_close()
-        fetch = crud.Fetch(self.engine)
+        fetch = crud.Fetch(self.engine, self.journal_journal_path)
         interfaces = fetch.get_all_interfaces()
         self.assertEqual(len(posts.ALL), len(interfaces))
         machines = fetch.get_all()
@@ -170,7 +181,7 @@ class TestModel(unittest.TestCase):
         self.assertEqual(587, line_nb)
 
     def test_08(self):
-        fetch = crud.Fetch(self.engine)
+        fetch = crud.Fetch(self.engine, self.journal_journal_path)
         all_data = fetch.get_all_interfaces()
         chassis_names = [k["chassis_name"] for k in all_data]
         self.assertEqual(4, chassis_names.count(None))
