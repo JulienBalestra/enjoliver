a371c71bc25c765bac371666525dca332d1d679e
JulienBalestra <julien.balestra@gmail.com>
2016-12-03 22:37:59 +0100

    Fix testing suite

--- app/tests/euid/test_kvm_player.py
+++ app/tests/euid/test_kvm_player.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 
 import kvm_player
 
@@ -15,7 +16,29 @@ class TestKernelVirtualMachinePlayer(kvm_player.KernelVirtualMachinePlayer):
         cls.pause(cls.wait_setup_teardown)
 
     def test(self):
-        pass
+        marker = "%s" % TestKernelVirtualMachinePlayer.__name__.lower()
+        destroy, undefine = ["virsh", "destroy", "%s" % marker], ["virsh", "undefine", "%s" % marker]
+        try:
+            virt_install = [
+                "virt-install",
+                "--name",
+                "%s" % marker,
+                "--network=bridge:rack0,model=virtio",
+                "--memory=1024",
+                "--vcpus=1",
+                "--pxe",
+                "--disk",
+                "none",
+                "--os-type=linux",
+                "--os-variant=generic",
+                "--noautoconsole",
+                "--boot=network"
+            ]
+            self.virsh(virt_install, assertion=True)
+            self.pause(5)
+        finally:
+            self.virsh(destroy), os.write(1, "\r")
+            self.virsh(undefine), os.write(1, "\r")
 
 
 # This have to raise
