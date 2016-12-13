d3ad3271deb677ecec1e5c4e77c890fcad8704fb
Julien Balestra <julien.balestra@blablacar.com>
2016-12-13 10:28:31 +0100

    Refactoring the bakup route

--- app/api.py
+++ app/api.py
@@ -157,10 +157,21 @@ def discovery_get():
 
 @application.route('/backup/db', methods=['POST'])
 def backup_database():
+    """
+    Backup the db by copying it and uploading to a S3 bucket
+    During the copy of the db a key is set inside the werkzeug cache to avoid writes
+        This key have a TTL and is always unset whatever the status of the backup
+    The application config contains the needed keys for the operation:
+        - BACKUP_BUCKET_NAME
+        - BACKUP_BUCKET_DIRECTORY
+        - DB_PATH
+        - BACKUP_LOCK_KEY
+    :return: Summary of the operation ; copy and upload keys summarize the result success
+    """
     start = time.time()
     now_rounded = int(math.ceil(start))
     dest_s3 = "%s/%s.db" % (application.config["BACKUP_BUCKET_DIRECTORY"], now_rounded)
-    db_path = application.config["DB_PATH"].replace("sqlite:///", "")
+    db_path = application.config["DB_PATH"]
     bucket_name = application.config["BACKUP_BUCKET_NAME"]
     b = {
         "copy": False,
@@ -171,23 +182,25 @@ def backup_database():
         "bucket_name": bucket_name,
         "bucket_uri": "s3://%s/%s" % (bucket_name, dest_s3) if application.config[
             "BACKUP_BUCKET_NAME"] else None,
-        "size": 0,
+        "size": None,
         "ts": now_rounded,
         "backup_duration": None,
         "lock_duration": None,
+        "already_locked": False,
     }
     if cache.get(application.config["BACKUP_LOCK_KEY"]):
+        b["already_locked"] = True
         return jsonify(b)
+
     try:
         source_st = os.stat(b["source_fs"])
         timeout = math.ceil(source_st.st_size / (1024 * 1024.))
-        app.logger.info("Backup lock key set for %ss" % timeout)
+        app.logger.info("Backup lock key set with timeout == %ss" % timeout)
         cache.set(application.config["BACKUP_LOCK_KEY"], b["dest_fs"], timeout=timeout)
         libc.sync()
         shutil.copy2(db_path, b["dest_fs"])
         dest_st = os.stat(b["dest_fs"])
-        b["size"] = dest_st.st_size
-        b["copy"] = True
+        b["size"], b["copy"] = dest_st.st_size, True
     except Exception as e:
         app.logger.error("<%s %s>: %s" % (e, type(e), e.message))
     finally:
@@ -200,7 +213,7 @@ def backup_database():
             app.logger.error("copy is False: %s" % b["dest_fs"])
             raise IOError(b["dest_fs"])
 
-        so = s3.S3Operator(application.config["BACKUP_BUCKET_NAME"])
+        so = s3.S3Operator(b["bucket_name"])
         so.upload(b["dest_fs"], b["dest_s3"])
         b["upload"] = True
     except Exception as e:
@@ -244,7 +257,8 @@ def boot_ipxe():
 
     except Exception as e:
         flask_uri = application.config["BOOTCFG_URI"]
-        app.logger.warning("%s: fall back to BOOTCFG_URI: %s" % (e.message, flask_uri))
+        app.logger.error("<%s %s>: %s" % (e, type(e), e.message))
+        app.logger.warning("Fall back to BOOTCFG_URI: %s" % flask_uri)
 
     response = \
         "#!ipxe\n" \
