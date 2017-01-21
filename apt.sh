ff3754d64473f5c2b12092160ce4688cbfaad5b6
JulienBalestra <julien.balestra@gmail.com>
2017-01-21 19:26:39 +0100

    Adapt the environment

--- apt.sh
+++ apt.sh
@@ -4,15 +4,12 @@ tty -s
 if [ -z ${INSTALL} ] && [ $? -ne ]
 then
     DEBIAN_FRONTEND=noninteractive
-    INSTALL="-y"
+    INSTALL="-yq"
 fi
 
-apt-get update -q
+apt-get update -qq
 
-for p in curl python python-virtualenv qemu-kvm libvirt-bin virtinstall jq liblzma-dev mkisofs isolinux file npm make
-do
-    apt-get install ${INSTALL} $p
-done
+apt-get install ${INSTALL} curl python python-dev python-virtualenv qemu-kvm libvirt-bin virtinstall jq liblzma-dev mkisofs isolinux file npm make
 
 # Go version have to be > 1.3
 go version || apt-get install ${INSTALL} golang
