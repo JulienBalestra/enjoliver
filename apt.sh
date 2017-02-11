79d62a74ea85d26e3b94b8e357f1371ec515faa9
JulienBalestra <julien.balestra@gmail.com>
2017-02-11 14:40:59 +0100

    Remove the ipxe packages

--- apt.sh
+++ apt.sh
@@ -6,7 +6,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update -qq
 
-apt-get install -y curl python python-dev python-virtualenv qemu-kvm libvirt-bin virtinst jq liblzma-dev mkisofs isolinux file npm make
+apt-get install -y curl python python-dev python-virtualenv qemu-kvm libvirt-bin virtinst jq file npm make
 
 # Go version have to be > 1.3
 go help || apt-get install -y golang
