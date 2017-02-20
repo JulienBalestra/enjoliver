c18ac6b89c06496b2e556b1cdccad96ea0151068
Julien Balestra <julien.balestra@gmail.com>
2017-02-20 17:59:50 +0100

    Golang 1.8

--- aci/aci-builder-hyperkube/runlevels/build/10.build.sh
+++ aci/aci-builder-hyperkube/runlevels/build/10.build.sh
@@ -15,7 +15,7 @@ apt-get update -q
 apt-get upgrade -y -qq
 apt-get install -y -qq git curl tar build-essential rsync
 
-curl -L https://storage.googleapis.com/golang/go1.7.5.linux-amd64.tar.gz -o /tmp/go.tar.gz
+curl -L https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz -o /tmp/go.tar.gz
 tar -C /usr/local -xzf /tmp/go.tar.gz
 
 export GOROOT=/usr/local/go
