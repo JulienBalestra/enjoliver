394d9fcb0deec674cf8019e91514a8fdbe3ccd83
JulienBalestra <julien.balestra@gmail.com>
2017-03-01 23:10:55 +0100

    Arrange the requirements

--- aci/aci-builder-enjoliver/runlevels/build/10.build.sh
+++ aci/aci-builder-enjoliver/runlevels/build/10.build.sh
@@ -8,8 +8,16 @@ export LC_ALL=C
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update -qq
-apt-get install -y -q curl python3.5 python3-dev build-essential python-virtualenv git file openssh-client tar npm libpq-dev
+apt-get install -y -q curl python3 python3-dev build-essential python-virtualenv git file openssh-client tar npm libpq-dev libssl-dev openssl
 ln -vs /usr/lib/python2.7/dist-packages/virtualenv.py /usr/local/bin/virtualenv
 chmod +x /usr/local/bin/virtualenv
+
+mkdir -pv /opt/python
+curl -Lf https://www.python.org/ftp/python/3.5.0/Python-3.5.0.tgz -o /opt/python/python.tar.gz
+cd /opt/python
+tar -xzf /opt/python/python.tar.gz --strip-components=1
+./configure
+make
+make install
 
 go version
