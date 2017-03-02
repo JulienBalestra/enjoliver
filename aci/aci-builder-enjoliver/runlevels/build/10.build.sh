f042e538578d3b149d5bac730c8c3ba9872d2da3
Julien Balestra <julien.balestra@gmail.com>
2017-03-02 12:59:54 +0100

    Installatin enjoliver over ACIs

--- aci/aci-builder-enjoliver/runlevels/build/10.build.sh
+++ aci/aci-builder-enjoliver/runlevels/build/10.build.sh
@@ -8,17 +8,11 @@ export LC_ALL=C
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update -qq
-apt-get install -y -q curl python3 python3-dev build-essential python-virtualenv git file openssh-client tar npm libpq-dev libssl-dev openssl
-ln -vs /usr/lib/python2.7/dist-packages/virtualenv.py /usr/local/bin/virtualenv
+apt-get install -y -q curl python3-dev python-virtualenv build-essential git file openssh-client tar npm libpq-dev
+
+ln -vsf /usr/lib/python2.7/dist-packages/virtualenv.py /usr/local/bin/virtualenv
 chmod +x /usr/local/bin/virtualenv
 
-mkdir -pv /opt/python
-VERSION=3.5.3
-curl -Lf https://www.python.org/ftp/python/${VERSION}/Python-${VERSION}.tgz -o /opt/python/python.tar.gz
-cd /opt/python
-tar -xzf /opt/python/python.tar.gz --strip-components=1
-./configure
-make
-make install
-
 go version
+
+python3.5 --version
