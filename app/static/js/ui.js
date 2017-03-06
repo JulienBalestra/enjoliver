5bbe7bc171ddc918b009185ea552ba65fabc3fdd
Julien Balestra <julien.balestra@gmail.com>
2017-03-06 15:42:22 +0100

    Remove the health from the dashboard

--- app/static/js/ui.js
+++ app/static/js/ui.js
@@ -27,45 +27,8 @@ function createMachineTable() {
     });
 }
 
-function recurseHealth(object, parent) {
-    var label, li, new_parent;
-
-    for (var key in object) {
-        if (object.hasOwnProperty(key)) {
-            if (typeof(object[key]) != "boolean") {
-                li = $("<li>").appendTo(parent);
-                $("<span class=\"label label-default\">").appendTo(li).text(key);
-                new_parent = $("<ul>").appendTo(parent);
-                recurseHealth(object[key], new_parent);
-                continue
-            }
-            li = $("<li>").appendTo(parent);
-            if (object[key] == true) {
-                $("<span class=\"label label-success\">").appendTo(li).text(key);
-            } else {
-                $("<span class=\"label label-danger\">").appendTo(li).text(key);
-            }
-        }
-    }
-}
-
-function createHealth() {
-    $.ajax({
-        url: "/healthz", success: function (response) {
-            recurseHealth(response, $("<ul>").appendTo($("#health_status")));
-        }, async: true
-    });
-}
-
 function main() {
     createMachineTable();
-    createHealth();
 }
 
 main();
-
-$(document).ready(function(){
-    $("#refresh-button").click(function () {
-        console.log("click catch");
-    });
-});
