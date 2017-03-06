20d637198d6f721b2c215cad3f759ecfa65fe540
Julien Balestra <julien.balestra@gmail.com>
2017-03-06 10:54:36 +0100

    Only validate the discoveryC

--- validate.py
+++ validate.py
@@ -8,12 +8,6 @@ class TestValidateMatchboxAssets(unittest.TestCase):
     matchbox = os.getenv("CHECK_MATCHBOX_PATH", "%s/matchbox" % cwd)
     assets = "%s/assets" % matchbox
 
-    def test_coreos(self):
-        rule = "%s/%s/serve" % (self.assets, self.test_coreos.__name__.replace("test_", ""))
-        list_dir = os.listdir(rule)
-        self.assertIn("coreos_production_pxe.vmlinuz", list_dir)
-        self.assertIn("coreos_production_pxe_image.cpio.gz", list_dir)
-
     def test_discoveryC(self):
         rule = "%s/%s/serve" % (self.assets, self.test_discoveryC.__name__.replace("test_", ""))
         list_dir = os.listdir(rule)
