a16bb1a69d6ce5cf38cf22b108b0e413a44efcd4
Julien Balestra <julien.balestra@gmail.com>
2017-03-07 16:36:07 +0100

    Use the ACI version

--- aci/aci-go/runlevels/build/10.build.sh
+++ aci/aci-go/runlevels/build/10.build.sh
@@ -8,7 +8,7 @@ export LC_ALL=C
 export DEBIAN_FRONTEND=noninteractive
 
 TARGET=/tmp/go.tar.gz
-curl -L https://storage.googleapis.com/golang/go1.7.5.linux-amd64.tar.gz -o ${TARGET}
+curl -L https://storage.googleapis.com/golang/go${ACI_VERSION}.linux-amd64.tar.gz -o ${TARGET}
 tar -C /usr/local -xzf ${TARGET}
 
 export GOROOT=/usr/local/go
