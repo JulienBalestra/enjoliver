0ce4b5ce384c51446511273323343884a39b3b50
JulienBalestra <julien.balestra@gmail.com>
2017-03-11 18:51:11 +0100

    Can watch the Enjoliver

--- operations/commands.py
+++ operations/commands.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3.5
 
 import argparse
+import datetime
 import json
 import os
+import sys
+import time
 
 import requests
 from kubernetes import client as kc
@@ -28,38 +31,105 @@ class EnjoliverCommandLine(object):
             self._k8s_control_plane_ips = self._get_ips_of_role("kubernetes-control-plane")
         return self._k8s_control_plane_ips
 
-    def display_etcd_members(self):
-        print(
-            "Etcd Members:\n",
-            "  Kubernetes:\n",
-            " " * 4 + "\n".join(["etcdctl --endpoint http://%s:2379" % k for k in self.etcd_member_ips]),
-            "\n"
-            "  Fleet:\n",
-            " " * 4 + "\n".join(["etcdctl --endpoint http://%s:4001" % k for k in self.etcd_member_ips]),
-            end="\n\n"
-        )
+    def _get_infos_from_raw_query(self, raw: str):
+        spl = raw.split("&")
+        name = ""
+        for i in spl:
+            if "mac=" in i:
+                mac = i[4:].replace("-", ":")
+                req = requests.get("%s/discovery/interfaces" % self.enj_uri)
+                req.close()
+                content = json.loads(req.content.decode())
+                for j in content:
+                    if j["mac"] == mac:
+                        name = j["fqdn"] if j["fqdn"] else j["ipv4"]
+                break
+        if not name:
+            raise AttributeError("return should be filled")
+        return name
 
-    def display_control_planes(self):
-        print(
-            "Kubernetes:\n",
-            " " * 2 + "\n".join(["kubectl -s %s:8080" % k for k in self.k8s_control_plane_ips]),
-            end="\n\n"
+    def summarize_locksmith(self):
+        lines = list()
+        lines.append("Locksmith:")
+        for i in range(len(self.etcd_member_ips)):
+            try:
+                req = requests.get(
+                    "http://%s:2379/v2/keys/coreos.com/updateengine/rebootlock/semaphore" % self.etcd_member_ips[i])
+                req.close()
+                if req.status_code == 404:
+                    lines.append("  Empty")
+                    break
+                content = req.content.decode()
+                sema = json.loads(content)
+                sema = json.loads(sema["node"]["value"])
+                holders = sema["holders"] if sema["holders"] else []
+                holder_hosts = []
+                for h in holders:
+                    holder_hosts.append(
+                        self._get_infos_from_raw_query(h)
+                    )
+                lines.append("  Available:  %d/%d" % (sema["semaphore"], sema["max"]))
+                lines.append("  Holders:    [%s]" % ",".join(holder_hosts))
 
-        )
+            except Exception as e:
+                if i + 1 < len(self.etcd_member_ips):
+                    print("ERROR %s for %s" % (type(e), i))
+                    continue
+                raise
+        return lines
 
-    def display_fleet(self):
-        print(
-            "Fleet:\n",
-            " " * 2 + "\n".join(["fleetctl --endpoint=http://%s:4001 --driver=etcd" % k for k in self.etcd_member_ips]),
-            end="\n\n"
-        )
+    def summarize_etcd_members(self):
+        lines = list()
+        lines.append("Etcd Members:")
+        lines.append("  Kubernetes:")
+        lines.append(" " * 4 + "\n".join(["etcdctl --endpoint http://%s:2379" % k for k in self.etcd_member_ips]))
+        lines.append("  Fleet:")
+        lines.append(" " * 4 + "\n".join(["etcdctl --endpoint http://%s:4001" % k for k in self.etcd_member_ips]))
+        return lines
 
-    def display_enjoliver_health(self):
+    def summarize_control_planes(self):
+        lines = list()
+        lines.append("Kubernetes:")
+        lines.append("  " + "\n".join(["kubectl -s %s:8080" % k for k in self.k8s_control_plane_ips]))
+        return lines
+
+    def summarize_fleet(self):
+        lines = list()
+        lines.append("Fleet:")
+        lines.append(
+            "  " + "\n".join(["fleetctl --endpoint=http://%s:4001 --driver=etcd" % k for k in self.etcd_member_ips]))
+        return lines
+
+    def summarize_enjoliver_health(self):
         req = requests.get("%s/healthz" % self.enj_uri)
         content = json.loads(req.content.decode())
-        print(
-            "Enjoliver Status:\n",
-            "  Healthy" if content["global"] else print(req.content.decode()), end="\n\n")
+        lines = list()
+        lines.append("Enjoliver:")
+        lines.append("  Healthy" if content["global"] else req.content.decode())
+        return lines
+
+    def summarize_lifecycle(self):
+        req = requests.get("%s/lifecycle/ignition" % self.enj_uri)
+        req.close()
+        ignition = json.loads(req.content.decode())
+        lines = list()
+        up_to_date = 0
+        for i in ignition:
+            if i["up-to-date"] is True:
+                up_to_date += 1
+
+        lines.append("Lifecycle:")
+        lines.append("  UpToDate:   %d/%d" % (up_to_date, (len(ignition))))
+
+        req = requests.get("%s/lifecycle/rolling" % self.enj_uri)
+        req.close()
+        rolling = json.loads(req.content.decode())
+        rolling_nb = 0
+        for j in rolling:
+            if j["enable"] is True:
+                rolling_nb += 1
+        lines.append("  AutoUpdate: %d/%d" % (rolling_nb, len(ignition)))
+        return lines
 
     def _get_ips_of_role(self, role):
         req = requests.get("%s/scheduler/%s" % (self.enj_uri, role))
@@ -69,11 +139,16 @@ class EnjoliverCommandLine(object):
             ips.append("%s" % i["ipv4"])
         return ips
 
-    def display(self):
-        self.display_enjoliver_health()
-        self.display_etcd_members()
-        self.display_control_planes()
-        self.display_fleet()
+    def get_all_summaries(self):
+        return \
+            self.summarize_enjoliver_health() + \
+            self.summarize_lifecycle() + \
+            self.summarize_locksmith() + \
+            [""] + \
+            self.summarize_etcd_members() + \
+            self.summarize_control_planes() + \
+            self.summarize_fleet() + \
+            [""]
 
     @property
     def k8s_apiserver_endpoint(self):
@@ -114,7 +189,6 @@ class EnjoliverCommandLine(object):
             assert req.status_code == 200
             print(" " * 4 + "%d/%d %s" % (i + 1, len(mac), m))
         print("")
-        self.display()
 
 
 if __name__ == '__main__':
@@ -128,6 +202,10 @@ if __name__ == '__main__':
         help="Kubernetes api-server endpoint 172.20.0.2:8080"
     )
     parser.add_argument(
+        '--watch', "-w", type=int, default=0,
+        help="Delay between polling"
+    )
+    parser.add_argument(
         'task', type=str, default="display",
         help="Task to run [ls, restore]"
     )
@@ -137,7 +215,27 @@ if __name__ == '__main__':
     )
     task = parser.parse_args().task
     if task == "restore":
-        enjoliver.display_enjoliver_health()
+        enjoliver.summarize_enjoliver_health()
         enjoliver.restore_schedule_of()
+        enjoliver.get_all_summaries()
     elif task == "ls":
-        enjoliver.display()
+        delay = parser.parse_args().watch
+        if delay:
+            watch = ""
+            while True:
+                try:
+                    summary = enjoliver.get_all_summaries()
+                    if summary != watch:
+                        print("\n" + "-" * (len(max(summary, key=len)) + 1))
+                        print("\n".join(summary))
+                        watch = summary
+
+                    os.write(1, b"\r")
+                    print("-> %s next watch in %ds" % (datetime.datetime.now().strftime("%H:%M:%S"), delay), end="")
+                    sys.stdout.flush()
+                    time.sleep(delay)
+                except KeyboardInterrupt:
+                    os.write(1, b"\nExited\n")
+                    break
+        else:
+            print(enjoliver.get_all_summaries())
