96ab86b25595262900d38045bd9e0add04978688
Julien Balestra <julien.balestra@gmail.com>
2017-05-03 11:04:02 +0200

    Upgrade the helm/tiller version

--- aci/aci-tiller/runlevels/builder/10.builder.sh
+++ aci/aci-tiller/runlevels/builder/10.builder.sh
@@ -10,6 +10,13 @@ export LC_ALL=C
 export GOPATH=/go
 export PATH=${GOPATH}/bin:/usr/local/go/bin:${PATH}
 
+apt-get update -qq
+apt-get install -y mercurial
+
+GLIDE_VERSION=0.12.3
+curl -L https://github.com/Masterminds/glide/releases/download/v${GLIDE_VERSION}/glide-v${GLIDE_VERSION}-linux-amd64.tar.gz | \
+    tar -C /usr/local/bin -xzvf - --strip-components=1
+
 mkdir -pv ${GOPATH}/src/k8s.io/helm
 cd ${GOPATH}/src/k8s.io/helm
 
