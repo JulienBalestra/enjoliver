1007a9a0bb43197fd94a150ca8b926d40b01def9
JulienBalestra <julien.balestra@gmail.com>
2017-05-07 17:30:34 +0200

    Improve scenarii to use auto scaling

---
+++ app/tests/helm.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+which kubectl > /dev/null
+if [ $? -eq 0 ]
+then
+    KUBECTL=kubectl
+else
+    KUBECTL="../../hyperkube/hyperkube kubectl"
+fi
+
+cd $(dirname $0)
+
+TILLER=$(${KUBECTL} get ep -l app=tiller -n kube-system \
+    -o jsonpath='{.items[*].subsets[*].addresses[0].ip}:{.items[*].subsets[*].ports[0].port}')
+exec ../../runtime/helm/helm --host ${TILLER} $@
