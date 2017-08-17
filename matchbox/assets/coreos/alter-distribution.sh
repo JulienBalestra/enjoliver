85b0b7ba0ea5bf314d8686c430e578e6a3318396
JulienBalestra <julien.balestra@gmail.com>
2017-08-17 18:20:58 +0200

    Alter the distribution

--- matchbox/assets/coreos/alter-distribution.sh
+++ matchbox/assets/coreos/alter-distribution.sh
@@ -9,13 +9,15 @@ cd $(dirname $0)
 COREOS_DIRECTORY=$(pwd -P)
 cd ${COREOS_DIRECTORY}/${VERSION}
 
-P9=${COREOS_DIRECTORY}/${VERSION}/p9
+USR_A=${COREOS_DIRECTORY}/${VERSION}/usr-a
 
-mkdir -pv {squashfs,initrd} ${P9}
+mkdir -pv {squashfs,initrd} ${USR_A}
 
 bzip2 -d coreos_production_image.bin.bz2
+${COREOS_DIRECTORY}/disk.py rw
+
 LOOP=$(losetup --find --show --partscan coreos_production_image.bin)
-mount /dev/loop0p9 ${P9}
+mount ${LOOP}p3 ${USR_A}
 
 gunzip --force coreos_production_pxe_image.cpio.gz
 cd initrd
@@ -29,41 +31,45 @@ EXCLUDES="--exclude rootfs/dgr --exclude rootfs/etc --exclude rootfs/tmp"
 
 ETCD_ACI=$(ls ${ACI_PATH}/etcd/etcd-*-linux-amd64.aci | head -n 1)
 tar -C squashfs-root/ -xvf ${ETCD_ACI} rootfs/usr/bin --strip 2 ${EXCLUDES}
-tar -C ${P9}/usr -xvf ${ETCD_ACI} rootfs/usr/bin --strip 2 ${EXCLUDES}
+tar -C ${USR_A}/ -xvf ${ETCD_ACI} rootfs/usr/bin --strip 2 ${EXCLUDES}
 
 VAULT_ACI=$(ls ${ACI_PATH}/vault/vault-*-linux-amd64.aci | head -n 1)
 tar -C squashfs-root/ -xvf ${VAULT_ACI} rootfs/usr/ --strip 2 ${EXCLUDES}
-tar -C ${P9}/usr -xvf ${VAULT_ACI} rootfs/usr/ --strip 2 ${EXCLUDES}
+tar -C ${USR_A}/ -xvf ${VAULT_ACI} rootfs/usr/ --strip 2 ${EXCLUDES}
 
 IPROUTE2_ACI=$(ls ${ACI_PATH}/iproute2/iproute2-*-linux-amd64.aci | head -n 1)
 tar -C squashfs-root/ -xvf ${IPROUTE2_ACI} rootfs/usr/bin --strip 2 ${EXCLUDES}
-tar -C ${P9}/usr -xvf ${IPROUTE2_ACI} rootfs/usr/bin --strip 2 ${EXCLUDES}
+tar -C ${USR_A}/ -xvf ${IPROUTE2_ACI} rootfs/usr/bin --strip 2 ${EXCLUDES}
 
 FLEET_ACI=$(ls ${ACI_PATH}/fleet/fleet-*-linux-amd64.aci | head -n 1)
 tar -C squashfs-root/ -xvf ${FLEET_ACI} rootfs/usr/bin --strip 2 ${EXCLUDES}
-tar -C ${P9}/usr -xvf ${FLEET_ACI} rootfs/usr/bin --strip 2 ${EXCLUDES}
-
-HYPERKUBE_ACI=$(ls ${ACI_PATH}/hyperkube/hyperkube-*-linux-amd64.aci | head -n 1)
-tar -C squashfs-root/bin -xvf ${HYPERKUBE_ACI} rootfs/ --strip 1 ${EXCLUDES}
-tar -C ${P9}/usr/bin -xvf ${HYPERKUBE_ACI} rootfs/ --strip 1 ${EXCLUDES}
+tar -C ${USR_A}/ -xvf ${FLEET_ACI} rootfs/usr/bin --strip 2 ${EXCLUDES}
 
 RKT_ACI=$(ls ${ACI_PATH}/rkt/rkt-*-linux-amd64.aci | head -n 1)
 tar -C squashfs-root/ -xvf ${RKT_ACI} rootfs/usr --keep-directory-symlink --strip 2 ${EXCLUDES}
-tar -C ${P9}/usr -xvf ${RKT_ACI} rootfs/usr --keep-directory-symlink --strip 2 ${EXCLUDES}
+tar -C ${USR_A}/ -xvf ${RKT_ACI} rootfs/usr --keep-directory-symlink --strip 2 ${EXCLUDES}
 
 CNI_ACI=$(ls ${ACI_PATH}/cni/cni-*-linux-amd64.aci | head -n 1)
 tar -C squashfs-root/ -xvf ${CNI_ACI} rootfs/usr --strip 2 ${EXCLUDES}
-tar -C ${P9}/usr -xvf ${CNI_ACI} rootfs/usr --strip 2 ${EXCLUDES}
+tar -C ${USR_A}/ -xvf ${CNI_ACI} rootfs/usr --strip 2 ${EXCLUDES}
+
+HYPERKUBE_ACI=$(ls ${ACI_PATH}/hyperkube/hyperkube-*-linux-amd64.aci | head -n 1)
+tar -C squashfs-root/bin -xvf ${HYPERKUBE_ACI} rootfs/ --strip 1 ${EXCLUDES}
+tar -C ${USR_A}/bin -xvf ${HYPERKUBE_ACI} rootfs/ --strip 1 ${EXCLUDES}
 
 sync
+
+umount ${USR_A}
+losetup -d ${LOOP}
+
+${COREOS_DIRECTORY}/disk.py ro
+bzip2 -z coreos_production_image.bin -9
+
+# Do the signing
 
 mksquashfs squashfs-root/ ../initrd/usr.squashfs -noappend -always-use-fragments
 cd ../initrd
 find . | cpio -o -H newc | gzip -9 > ../coreos_production_pxe_image.cpio.gz
 cd ../
 
-umount ${P9}
-losetup -d ${LOOP}
-bzip2 -z coreos_production_image.bin -9
-
-rm -rf squashfs initrd coreos_production_pxe_image.cpio ${P9}
+rm -rf squashfs initrd coreos_production_pxe_image.cpio ${USR_A}
---
+++ matchbox/assets/coreos/disk.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import os.path
+import argparse
+
+READ_ONLY = chr(0xff)
+READ_WRITE = chr(0x00)
+MOUNT_OPTION_OFFSET = 0x464 + 3
+USER_A = 138412032
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def change_flag(image_path, read_only=True):
+    with open(image_path, 'r+') as f:
+        f.seek(USER_A + MOUNT_OPTION_OFFSET)
+        f.write(READ_ONLY if read_only else READ_WRITE)
+
+
+def get_current_flag(image_path):
+    with open(image_path, 'r') as f:
+        f.seek(USER_A + MOUNT_OPTION_OFFSET)
+        return f.read(1)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='DiskAlter')
+    parser.add_argument('task', type=str, choices=["rw", "ro"],
+                        help="Choose the task to run")
+    task = parser.parse_args().task
+    version = os.getenv("VERSION")
+    if not version:
+        raise EnvironmentError("VERSION environment variable is not fill")
+
+    image_path = os.path.join(FILE_DIR, version, "coreos_production_image.bin")
+    current_flag = get_current_flag(image_path)
+    if current_flag != READ_WRITE and current_flag != READ_ONLY:
+        raise IOError("%s flag at %d is incoherent: %r" % (image_path, USER_A, current_flag))
+
+    if current_flag == READ_ONLY and task == "ro":
+        print("Nothing to be done")
+        exit(0)
+
+    print("Currently:\n%s: %s\n" % (image_path, ("ro" if current_flag == READ_ONLY else "rw")))
+    change_flag(image_path, True if task == "ro" else False)
+    current_flag = get_current_flag(image_path)
+    print("End with:\n%s: %s" % (image_path, ("ro" if current_flag == READ_ONLY else "rw")))
