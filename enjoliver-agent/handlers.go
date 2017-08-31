3c2a6da173fb9d0a884a1553c1d22f092efdfe8e
JulienBalestra <julien.balestra@gmail.com>
2017-08-31 19:09:06 +0200

    Sadly hack some Kubernetes recurrent issues

--- enjoliver-agent/handlers.go
+++ enjoliver-agent/handlers.go
@@ -3,11 +3,12 @@ package main
 import (
 	"encoding/json"
 	"github.com/golang/glog"
+	"io/ioutil"
 	"net/http"
 )
 
 func (run *Runtime) handlerHealthz(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
+	if r.Method == http.MethodGet {
 		health := run.GetComponentStatus()
 		if len(health.Errors) != 0 {
 			glog.Errorf("fail to get health status for: %s", health.Errors)
@@ -32,7 +33,7 @@ func writeResponse(w http.ResponseWriter, v interface{}) {
 }
 
 func (run *Runtime) handlerVersion(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
+	if r.Method == http.MethodGet {
 		binaries := GetComponentVersion()
 		if len(binaries.Errors) != 0 {
 			glog.Errorf("fail to get health status for: %s", binaries.Errors)
@@ -40,4 +41,54 @@ func (run *Runtime) handlerVersion(w http.ResponseWriter, r *http.Request) {
 		}
 		writeResponse(w, &binaries)
 	}
+}
+
+// [ img1, img2, img3 ]
+func (run *Runtime) handlerHackRktFetch(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		var b []byte
+		var images []string
+
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			glog.Errorf("fail to read body: %s", err)
+			w.WriteHeader(400)
+			return
+		}
+		err = json.Unmarshal(b, &images)
+		if err != nil {
+			glog.Errorf("fail to unmarshal body %q: %s", string(b), err)
+			w.WriteHeader(400)
+			return
+		}
+		errList := run.RktFetch(images)
+		if len(errList) > 0 {
+			glog.Errorf("fail to proceed to request: %s", errList)
+			w.WriteHeader(500)
+			var errListStr []string
+			for _, elt := range errList {
+				errListStr = append(errListStr, elt.Error())
+			}
+			b, err := json.Marshal(&errListStr)
+			if err != nil {
+				glog.Errorf("fail to marshal error list: %s", err)
+				return
+			}
+			w.Write(b)
+		}
+		return
+	}
+	w.WriteHeader(405)
+}
+
+func (run *Runtime) handlerHackSystemdRestartKubernetesStack(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		err := run.RestartKubernetes()
+		if err != nil {
+			glog.Errorf("fail to proceed: %s", err)
+			w.WriteHeader(500)
+		}
+		return
+	}
+	w.WriteHeader(405)
 }
---
+++ enjoliver-agent/rkt_fetch.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/golang/glog"
+	"io/ioutil"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+const insecure_options = "--insecure-options=all"
+
+func fetchOneImage(image string, ch chan error) {
+	commandLine := []string{"rkt", "fetch"}
+	if flag.Lookup(RktFetchInsecure).Value.String() == "true" {
+		commandLine = append(commandLine, insecure_options)
+	}
+	commandLine = append(commandLine, image)
+	cmd := exec.Command(commandLine[0], commandLine[1:]...)
+	stderrReader, err := cmd.StderrPipe()
+	if err != nil {
+		glog.Errorf("fail to open pipe for stderr: %s", err)
+	}
+	time.AfterFunc(time.Minute*2, func() {
+		cmd.Process.Kill()
+	})
+	cmd.Start()
+	state, err := cmd.Process.Wait()
+	if err != nil {
+		glog.Errorf("fail run command %s: %s", strings.Join(commandLine, " "), err)
+	}
+	stderr, err := ioutil.ReadAll(stderrReader)
+	if err != nil {
+		glog.Errorf("fail to read from stderr %s: %s", strings.Join(commandLine, " "), err)
+	}
+	if len(stderr) > 0 {
+		glog.Warningf("%s with stderr: %s", strings.Join(commandLine, " "), stderr)
+	}
+	if state.Success() == true {
+		ch <- nil
+		return
+	}
+	glog.Errorf("%s result fail", image)
+	ch <- fmt.Errorf("%s result fail", image)
+}
+
+func (run *Runtime) RktFetch(images []string) []error {
+	ch := make(chan error)
+	defer close(ch)
+	var results []error
+	var err error
+	for _, i := range images {
+		go fetchOneImage(i, ch)
+	}
+	for range images {
+		err = <-ch
+		if err != nil {
+			results = append(results, err)
+		}
+	}
+
+	return results
+}
