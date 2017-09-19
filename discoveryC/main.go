efe29c05e121936dd845bad7378e0412165a3511
JulienBalestra <julien.balestra@gmail.com>
2017-09-19 17:00:29 +0200

    Adapt the return code of discovery Client

--- discoveryC/main.go
+++ discoveryC/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"github.com/golang/glog"
+	"os"
 )
 
 func main() {
@@ -14,18 +15,21 @@ func main() {
 	c, err := CreateConfig()
 	if err != nil {
 		glog.Errorf("fail to create config: %s", err)
+		os.Exit(1)
 		return
 	}
 
 	data, err := c.CollectData()
 	if err != nil {
 		glog.Errorf("fail to collect data: %s", err)
+		os.Exit(2)
 		return
 	}
 
 	err = c.PostToDiscovery(data)
 	if err != nil {
 		glog.Errorf("fail to send data: %s", err)
+		os.Exit(3)
 	}
 	glog.Flush()
 	return
